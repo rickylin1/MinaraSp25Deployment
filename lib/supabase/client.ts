43e8a6b5a8546a72c0b98b9c2041c77f0cae116f
rickylin1 <pandanation33@gmail.com>
2025-04-27 15:30:39 -0500

    remove debug statements

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,10 +1,6 @@
 import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
-  console.log('SUPABASE_URL:', process.env.NEXT_PUBLIC_SUPABASE_URL);
-  console.log('SUPABASE_ANON_KEY:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-
-
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
